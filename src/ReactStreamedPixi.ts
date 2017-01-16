3ffbdaa0436a1a6e259595aabccd4da715102311
devdoomari.ubuntu.tablet <devdoomari@gmail.com>
2017-01-16 13:36:40 +0900

    embed ReactPixi inside

--- src/ReactStreamedPixi.ts
+++ src/ReactStreamedPixi.ts
@@ -2,6 +2,26 @@ import StreamedGraphics, {
   GraphicsStreamType,
 } from './StreamedGraphics';
 
+import * as reactPixi from 'react-pixi';
+
+export type StagePropsType = reactPixi.StagePropsType;
+export const Stage = reactPixi.Stage;
+
+export type TilingSpritePropsType = reactPixi.TilingSpritePropsType;
+export const TilingSprite = reactPixi.TilingSprite;
+
+export type TextPropsType = reactPixi.TextPropsType;
+export const Text = reactPixi.Text;
+
+export type DisplayObjectContainerPropsType = reactPixi.DisplayObjectContainerPropsType;
+export const DisplayObjectContainer = reactPixi.DisplayObjectContainer;
+
+export const CustomPixiComponentClass = reactPixi.CustomPixiComponentClass;
+export interface CustomPixiComponentClassFactory<CustomProps, PixiComponent>
+  extends reactPixi.CustomPixiComponentClassFactory<CustomProps, PixiComponent> {};
+export const CustomPIXIComponent = reactPixi.CustomPIXIComponent;
+
+
 export {
   StreamedGraphics,
   GraphicsStreamType,
