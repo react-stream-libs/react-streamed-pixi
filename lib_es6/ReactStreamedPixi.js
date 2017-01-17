9caec1db5a2fc4128399c46d885224d93452d9db
devdoomari.circleci <devdoomari@gmail.com>
2017-01-17 14:32:41 +0000

    generated from:48aa806d62eb868fa43b22224221ee34d648fc1e triggered by:devdoomari [ci skip]

---
+++ lib_es6/ReactStreamedPixi.js
@@ -0,0 +1,15 @@
+"use strict";
+const StreamedGraphics_1 = require("./StreamedGraphics");
+exports.StreamedGraphics = StreamedGraphics_1.default;
+const drawables = require("./StreamedGraphics/drawables");
+exports.drawables = drawables;
+const StreamedStage_1 = require("./StreamedStage");
+exports.StreamedStage = StreamedStage_1.default;
+const reactPixi = require("react-pixi");
+exports.Stage = reactPixi.Stage;
+exports.TilingSprite = reactPixi.TilingSprite;
+exports.Text = reactPixi.Text;
+exports.DisplayObjectContainer = reactPixi.DisplayObjectContainer;
+exports.CustomPixiComponentClass = reactPixi.CustomPixiComponentClass;
+;
+exports.CustomPIXIComponent = reactPixi.CustomPIXIComponent;
