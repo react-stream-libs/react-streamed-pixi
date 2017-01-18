34ca3f485c3607bf8f822d37bb7b17fe4adb9713
devdoomari.circleci <devdoomari@gmail.com>
2017-01-18 03:30:09 +0000

    generated from:5ecec831a43f6ae8815d882ee5d08a7a5d673efd triggered by:devdoomari [ci skip]

---
+++ lib_es6/StreamedGraphics/drawables/Line.js
@@ -0,0 +1,18 @@
+"use strict";
+const lineStyleStore_1 = require("./lineStyleStore");
+class Line {
+    constructor(args) {
+        this.props = args;
+    }
+    drawOn(g) {
+        const prevLineStyle = lineStyleStore_1.getLineStyle(g);
+        lineStyleStore_1.setLineStyle(g, this.props);
+        g.beginFill(0, 0);
+        g.moveTo(this.props.x1, this.props.y1);
+        g.lineTo(this.props.x2, this.props.y2);
+        g.endFill();
+        lineStyleStore_1.setLineStyle(g, prevLineStyle);
+    }
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Line;
---
+++ lib_es6/StreamedGraphics/drawables/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const Circle_1 = require("./Circle");
+exports.Circle = Circle_1.default;
+const Rectangle_1 = require("./Rectangle");
+exports.Rectangle = Rectangle_1.default;
+const Layer_1 = require("./Layer");
+exports.Layer = Layer_1.default;
